a3b0254daaf3d8ce129eebffc2321441472a21f5
aidyn1984 <71663287+aidyn1984@users.noreply.github.com>
2021-04-29 01:28:28 -0500

    commit tree 7555d5cb26dbcadcff22fd7ea61ac269ca2f60eb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+// Write your code here!
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById('text').innerHTML = 'This is really cool!';
+}
